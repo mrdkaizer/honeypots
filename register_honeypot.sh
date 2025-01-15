344ea498f537ec6dac39c18880d96fe56437ba0e
Demetris Kaizer <mrdkaizer@gmail.com>
2025-01-15 21:30:19 +0200

    add register honeypot endpoint

---
+++ register_honeypot.sh
@@ -0,0 +1,13 @@
+
+DB_PATH="/opt/chnserver/storage/chnserver/sqlite/mhn.db"
+read -r -p "Enter the server hostname (e.g. https://csirt-cy.com): " HOSTNAME
+read -r -p "Enter the bearer token: " TOKEN
+
+API_KEY=$(sqlite3 "${DB_PATH}" "SELECT api_key FROM api_key LIMIT 1;")
+
+echo "$API_KEY"
+
+curl --location "$HOSTNAME/api/honeypot-register" \
+--header 'Accept: application/json' \
+--header "Authorization: Bearer $TOKEN" \
+--form 'api_key="test3"'
