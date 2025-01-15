8ee3dfbfaca234c78c4148354a1b9e2c2f2109f7
Demetris Kaizer <mrdkaizer@gmail.com>
2025-01-15 21:33:23 +0200

    minor

--- register_honeypot.sh
+++ register_honeypot.sh
@@ -3,6 +3,8 @@ DB_PATH="/opt/chnserver/storage/chnserver/sqlite/mhn.db"
 read -r -p "Enter the server hostname (e.g. https://csirt-cy.com): " HOSTNAME
 read -r -p "Enter the bearer token: " TOKEN
 
+
+sudo apt-get update && sudo apt-get install -y sqlite3
 API_KEY=$(sqlite3 "${DB_PATH}" "SELECT api_key FROM api_key LIMIT 1;")
 
 echo "$API_KEY"
@@ -10,4 +12,4 @@ echo "$API_KEY"
 curl --location "$HOSTNAME/api/honeypot-register" \
 --header 'Accept: application/json' \
 --header "Authorization: Bearer $TOKEN" \
---form 'api_key="test3"'
+--form "api_key=$API_KEY"
